584429249a275b4a0b512862fb4fd127a5ad20ff
TottiPuc <dayan3846@gmail.com>
2019-10-26 17:08:07 -0500

    nuevo condicional que verifica si existen nombres propios escritos con mayusculas ej Texas

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -9,6 +9,8 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data: # nueva funcionalidad verifica si hay nombres propios que empiezan por mayusculas 
+        return data[word.title()]
     elif len(get_close_matches(word, data.keys(), cutoff=0.6)) > 0: # verifivar si existen coincidencias 
         newWord = input("Did you mean: '{}' instead? [y/n]".format(get_close_matches(word, data.keys(), cutoff=0.6)[0]))
         if newWord == "y":
