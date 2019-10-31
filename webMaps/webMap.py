fb70cf964f0c44c9262c7bfbe5dee37175081c1a
TottiPuc <dayan3846@gmail.com>
2019-10-31 17:08:05 -0500

    agregar panel de control con LayerControl de folium

--- webMaps/webMap.py
+++ webMaps/webMap.py
@@ -6,8 +6,10 @@ import pandas
 #objeto map con las coordenadas del punto a graficar 
 map = folium.Map(location=[1.205717, -77.285892],zoom_start=9, tiles="Stamen Terrain")
 
-# objeto que permite crear el grupo de puntos o marcadores a ser adicionados al mapa 
-fg = folium.FeatureGroup("My map") 
+# objeto que permite crear el grupo de puntos o marcadores de volcanes a ser adicionados al mapa 
+fgv = folium.FeatureGroup("Volcanes") 
+# objeto que permite crear los poligonos y la cantidad de habitantes en el mapa 
+fgp = folium.FeatureGroup("Populacion")
 
 #estilar los popups con funciones html
 
@@ -36,12 +38,16 @@ pop = list(data["NOMBRE"])
 
 for lt, ln, ele,name in zip(lat,lon,ele,pop):
      iframe = folium.IFrame(html=html.format(name,name,ele),width=300,height=100)
-     fg.add_child(folium.Marker(location=[lt,ln], popup = folium.Popup(iframe), icon=folium.Icon(color=color_change(ele)))) 
+     fgv.add_child(folium.Marker(location=[lt,ln], popup = folium.Popup(iframe), icon=folium.Icon(color=color_change(ele)))) 
 
 #metodo para agregar poligonos al mapa creado
-fg.add_child(folium.GeoJson(data=(open('world.json','r',encoding='utf-8-sig').read()), 
+fgp.add_child(folium.GeoJson(data=(open('world.json','r',encoding='utf-8-sig').read()), 
 style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000
 else 'red'}))
 
-map.add_child(fg)
+#se crean las capas al mapa original adicionandolas como hijas
+map.add_child(fgv) # capa para volcanes
+map.add_child(fgp) # capa para populacion
+# se crea el panel de control de las capas
+map.add_child(folium.LayerControl())
 map.save("Mapa.html")
