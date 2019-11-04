2f75cff8b5cccfa3f9a819a52d0b6f2a264c9f52
TottiPuc <dayan3846@gmail.com>
2019-11-04 17:36:07 -0500

    funcionalidad para remover los sites del archivos hosts

--- web-Blocker/web_Blocker.py
+++ web-Blocker/web_Blocker.py
@@ -16,15 +16,28 @@ while True:
     # condicional que verifica si le fecha y hora actual es mayo a las 8 am y menos a las 4pm 
     if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16) :
         with open(host_temp,'r+') as file:
-            content = file.read() # lectura del archivo host
+            print("archivo ha ser modificado...{}".format(file.name))
+            content = file.read() # lectura del archivo host como un string
             for website in websites_list: # rerorrer la lista de websites a bloquear
                 # si los archivos existen no se hace nada caso contrario se los agrega al documento hosts
                 if website in content: 
                     pass
                 else:
-                    file.write("\n" + website )
+                    file.write("\n" + redirect + "\t" + website )
             print("working hours... you have to be working... :()")
     else:
+        with open(host_temp,'r+') as file:
+            print("archivo ha ser modificado...{}".format(file.name))
+            content = file.readlines() # lectura del archivo host como una lista 
+            file.seek(0)
+            for item in content:
+                """
+                averiguar si existe el website en cada una de las lineas del archivos leido como lista
+                si no existe se crea un nuevo archivo hosts con esta linea si existe se omite la linea
+                """
+                if not any(website in item for website in websites_list):
+                    file.write(item)
+            file.truncate()
         print("Fun hours.. you can do whatever you want.. :D")
     print(1)
     time.sleep(5)
