1b9cca8a3b3a745184f791a2fe616fb3293e8074
TottiPuc <dayan3846@gmail.com>
2019-11-04 20:07:36 -0500

    final version

--- web-Blocker/web_Blocker.py
+++ web-Blocker/web_Blocker.py
@@ -8,13 +8,13 @@ from datetime import datetime as dt # modulo usado para obtener fechas
 
 # configuracion del host y los sitios web que se desea bloquear
 host_temp ="hosts"
-hosts_path = r"C:\Windows\System32\drivers\etc"
+hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect ="127.0.0.1"
 websites_list =["www.facebook.com","facebook.com","www.youtube.com","youtube.com"]
 
 while True:
     # condicional que verifica si le fecha y hora actual es mayo a las 8 am y menos a las 4pm 
-    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,22) :
+    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,15) :
         with open(hosts_path,'r+') as file:
             print("archivo ha ser modificado...{}".format(file.name))
             content = file.read() # lectura del archivo host como un string
@@ -39,5 +39,4 @@ while True:
                     file.write(item)
             file.truncate()
         print("Fun hours.. you can do whatever you want.. :D")
-    print(1)
     time.sleep(5)
