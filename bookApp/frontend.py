a5ab1e2aba39fe760aff5182c381afad0e3ee7ea
TottiPuc <dayan3846@gmail.com>
2019-11-06 12:06:16 -0500

    version 2.1 frontEnd and BackEnd with POO

--- bookApp/frontend.py
+++ bookApp/frontend.py
@@ -17,131 +17,139 @@ from backend import Database
 
 database=Database("books.db")
 
-window = Tk()  # ventana principal
-window.wm_title("Paper Store")
-
 #callback functions
 #==========================================================================
-def view_parameters():
-    lista1.delete(0,END) # se blanquea el cuadro 
-    for row in database.view():
-        lista1.insert(END,row) # al elemento lista1 se le agregan las tuplas obtenidas de la funcion view del modulo database
+class Window(object):
+
+    def __init__(self,window):
+        self.window = window
+        self.window.wm_title("Paper Store")
+
+        #agregando widgets
+        #==========================================================================
+        #row0
+        label1 = Label(window,text="Title")
+        label1.grid(row=0,column=0)
+
+        label2 = Label(window, text="Author")
+        label2.grid(row=0,column=2)   
+
+        #row1
+        label3 = Label(window,text="Year")
+        label3.grid(row=1,column=0)    
+
+        label4 = Label(window, text="ISBN")
+        label4.grid(row=1,column=2)
 
 
-def search_parameters():
-    lista1.delete(0,END)
-    for row in database.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
-        lista1.insert(END,row)
-
-def insert_parameter():
-    lista1.delete(0,END)
-    database.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
-    lista1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
-
-def get_selected_row(event):
-    global selected_row # se hace global por el hecho del evento ser llamado dentro de la funcino delet_parameter
-    try:
-        index = lista1.curselection()[0] # con esto se obtiene el indice de la lista  de valores
-        selected_row =lista1.get(index)  # con este se obtiene el indice de la base de datos 
-        #colocar cada valor del item selecionado en las ventanas de entrada correspondientes
-        entry1.delete(0,END)
-        entry1.insert(END,selected_row[1])
-        entry2.delete(0,END)
-        entry2.insert(END,selected_row[2])
-        entry3.delete(0,END)
-        entry3.insert(END,selected_row[3])
-        entry4.delete(0,END)
-        entry4.insert(END,selected_row[4])
-        #print (index,selected_row)
-        #database.delete(selected_row) # se borra solo seleccionandolo de la lista
-        #view_parameters()
-        #return selected_row
-    except IndexError:
-        pass
-
-def delet_parameter():
-    database.delete(selected_row[0])
-    entry1.delete(0,END)
-    entry2.delete(0,END)
-    entry3.delete(0,END)
-    entry4.delete(0,END)
-    view_parameters()
-
-def update_parameter():
-    database.update(selected_row[0],title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
-    view_parameters()
-    entry1.delete(0,END)
-    entry2.delete(0,END)
-    entry3.delete(0,END)
-    entry4.delete(0,END)
+        self.title_text = StringVar()
+        self.entry1 = Entry(window,textvariable=self.title_text)
+        self.entry1.grid(row=0,column=1)
 
 
-#agregando widgets
-#==========================================================================
-#row0
-label1 = Label(window,text="Title")
-label1.grid(row=0,column=0)
-
-title_text = StringVar()
-entry1 = Entry(window,textvariable=title_text)
-entry1.grid(row=0,column=1)
-
-label2 = Label(window, text="Author")
-label2.grid(row=0,column=2)
-
-author_text=StringVar()
-entry2 = Entry(window,textvariable=author_text)
-entry2.grid(row=0,column=3)
-
-#row1
-label3 = Label(window,text="Year")
-label3.grid(row=1,column=0)
-
-year_text=StringVar()
-entry3 = Entry(window,textvariable=year_text)
-entry3.grid(row=1,column=1)
-
-label4 = Label(window, text="ISBN")
-label4.grid(row=1,column=2)
-
-isbn_text=StringVar()
-entry4 = Entry(window,textvariable=isbn_text)
-entry4.grid(row=1,column=3)
-
-#row 3 a head
-
-lista1 = Listbox(window, height=6, width=35)
-lista1.grid(row=2,column=0, rowspan=6, columnspan=2)
-
-#configrar el scrollbar  para que sea quien controla la lista1
-
-sb1 = Scrollbar(window)
-sb1.grid(row=2,column=2,rowspan=6)
-
-lista1.configure(yscrollcommand=sb1.set)
-sb1.configure(command=lista1.yview)
-
-lista1.bind('<<ListboxSelect>>', get_selected_row) # funcion que enlaza lo seleccionado con el evento del metodo delete del backend
-
-#configurando botones de entrada
-
-buton1=Button(window,text="View all",width=12,command=view_parameters)
-buton1.grid(row=2,column=3)
-
-buton2=Button(window,text="Search entry",width=12,command=search_parameters)
-buton2.grid(row=3,column=3)
-
-buton3=Button(window,text="Add entry",width=12,command=insert_parameter)
-buton3.grid(row=4,column=3)
-
-buton4=Button(window,text="Update",width=12,command=update_parameter)
-buton4.grid(row=5,column=3)
-
-buton5=Button(window,text="Delete",width=12,command=delet_parameter)
-buton5.grid(row=6,column=3)
-
-buton6=Button(window,text="Close",width=12,command=window.destroy)
-buton6.grid(row=7,column=3)
+        self.author_text=StringVar()
+        self.entry2 = Entry(window,textvariable=self.author_text)
+        self.entry2.grid(row=0,column=3)
 
 
+        self.year_text=StringVar()
+        self.entry3 = Entry(window,textvariable=self.year_text)
+        self.entry3.grid(row=1,column=1)
+
+
+        self.isbn_text=StringVar()
+        self.entry4 = Entry(window,textvariable=self.isbn_text)
+        self.entry4.grid(row=1,column=3)
+
+        #row 3 a head
+
+        self.lista1 = Listbox(window, height=6, width=35)
+        self.lista1.grid(row=2,column=0, rowspan=6, columnspan=2)
+
+        #configrar el scrollbar  para que sea quien controla la lista1
+
+        sb1 = Scrollbar(window)
+        sb1.grid(row=2,column=2,rowspan=6)
+
+        self.lista1.configure(yscrollcommand=sb1.set)
+        sb1.configure(command=self.lista1.yview)
+
+        self.lista1.bind('<<ListboxSelect>>', self.get_selected_row) # funcion que enlaza lo seleccionado con el evento del metodo delete del backend
+
+        #configurando botones de entrada
+
+        buton1=Button(window,text="View all",width=12,command=self.view_parameters)
+        buton1.grid(row=2,column=3)
+
+        buton2=Button(window,text="Search entry",width=12,command=self.search_parameters)
+        buton2.grid(row=3,column=3)
+
+        buton3=Button(window,text="Add entry",width=12,command=self.insert_parameter)
+        buton3.grid(row=4,column=3)
+
+        buton4=Button(window,text="Update",width=12,command=self.update_parameter)
+        buton4.grid(row=5,column=3)
+
+        buton5=Button(window,text="Delete",width=12,command=self.delet_parameter)
+        buton5.grid(row=6,column=3)
+
+        buton6=Button(window,text="Close",width=12,command=window.destroy)
+        buton6.grid(row=7,column=3)
+
+
+    def view_parameters(self):
+        self.lista1.delete(0,END) # se blanquea el cuadro 
+        for row in database.view():
+            self.lista1.insert(END,row) # al elemento lista1 se le agregan las tuplas obtenidas de la funcion view del modulo database
+
+
+    def search_parameters(self):
+        self.lista1.delete(0,END)
+        for row in database.search(self.title_text.get(),self.author_text.get(),self.year_text.get(),self.isbn_text.get()):
+            self.lista1.insert(END,row)
+
+    def insert_parameter(self):
+        self.lista1.delete(0,END)
+        database.insert(self.title_text.get(),self.author_text.get(),self.year_text.get(),self.isbn_text.get())
+        self.lista1.insert(END,(self.title_text.get(),self.author_text.get(),self.year_text.get(),self.isbn_text.get()))
+
+    def get_selected_row(self,event):
+        #global selected_row # se hace global por el hecho del evento ser llamado dentro de la funcino delet_parameter
+        try:
+            index = self.lista1.curselection()[0] # con esto se obtiene el indice de la lista  de valores
+            self.selected_row =self.lista1.get(index)  # con este se obtiene el indice de la base de datos 
+            #colocar cada valor del item selecionado en las ventanas de entrada correspondientes
+            self.entry1.delete(0,END)
+            self.entry1.insert(END,self.selected_row[1])
+            self.entry2.delete(0,END)
+            self.entry2.insert(END,self.selected_row[2])
+            self.entry3.delete(0,END)
+            self.entry3.insert(END,self.selected_row[3])
+            self.entry4.delete(0,END)
+            self.entry4.insert(END,self.selected_row[4])
+
+        except IndexError:
+            pass
+
+    def delet_parameter(self):
+        database.delete(self.selected_row[0])
+        self.entry1.delete(0,END)
+        self.entry2.delete(0,END)
+        self.entry3.delete(0,END)
+        self.entry4.delete(0,END)
+        self.view_parameters()
+
+    def update_parameter(self):
+        database.update(self.selected_row[0],self.title_text.get(),self.author_text.get(),self.year_text.get(),self.isbn_text.get())
+        self.view_parameters()
+        self.entry1.delete(0,END)
+        self.entry2.delete(0,END)
+        self.entry3.delete(0,END)
+        self.entry4.delete(0,END)
+
+
+   
+
+window = Tk()  # ventana principal
+Window(window)
 window.mainloop() # loop que mantiene la ventan abierta
